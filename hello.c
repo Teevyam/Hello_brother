2261ea47d594bafb54e8310fa61bb2f2e7b18dc5
Teev <teevyam@gmail.com>
2023-12-09 08:50:21 +0400

    fixed smth ergently.

--- hello.c
+++ hello.c
@@ -1,3 +1,7 @@
 Hello World!!!
 <link rel="stylesheet" href="bluestyle.css">
 New line added blah blah.
+now we add images.
+<div>img src="micar.jpeg" alt="Hello car not found"
+style="width:100%;max-width:960px"</div>
+this line shows how merging works.
