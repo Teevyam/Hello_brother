91ef6221653161c695b138fe6e9ab0084817d2b6
Teev <teevyam@gmail.com>
2023-12-09 08:59:24 +0400

    added new img

--- hello.c
+++ hello.c
@@ -5,3 +5,5 @@ now we add images.
 <div>img src="micar.jpeg" alt="Hello car not found"
 style="width:100%;max-width:960px"</div>
 this line shows how merging works.
+<div>img src="mycar.jpeg" alt="Hello car na mila"
+style="width:100%;max-width:640px"</div>
